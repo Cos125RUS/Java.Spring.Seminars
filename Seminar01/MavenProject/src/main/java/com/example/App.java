3af87bfd8108e5e469241dbd89add2d0aaaec9c2
Valerii_Panov <val250640@mail.ru>
2024-01-26 16:57:18 +1000

    seminars1-5

---
+++ Seminar01/MavenProject/src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import com.google.common.base.Joiner;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String[] words = {"Hello", "World"};
+        String message = Joiner.on(", ").join(words);
+        System.out.println(message);
+    }
+}
